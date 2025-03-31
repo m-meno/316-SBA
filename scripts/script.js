d10c005a6320f2052006d219e166394adafc6e0b
m-meno <mackenzie.meno@gmail.com>
2025-03-31 16:02:48 -0600

    Added save button

--- scripts/script.js
+++ scripts/script.js
@@ -4,8 +4,6 @@
 //Document fragment interface???? (only 2%);
 
 
-//Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
-
 //ex: adjust style of a button after it has been clicked
 
 
@@ -16,26 +14,42 @@ let gratitudeInput = document.getElementById("gratitudeInput");
 let gratitudeForm = document.getElementById("gratitudeForm");
 let gratitudeList = document.getElementById("gratitudeList");
 let headers = document.querySelector("h1");
+let saveButton = document.getElementById("save");
 
 headers.addEventListener(`mouseover`, handleMouseOver);
 
 gratitudeForm.addEventListener(`submit`, handleSubmit);
 
+saveButton.addEventListener(`click`, handleSave);
 
 
 function handleSubmit(e){
     e.preventDefault();
     let input = gratitudeInput.value;
-    if (input == ``){
+    if (input.trim() === ``){
         alert(`No input received`)
         return;
-    }
+    } else {
     let newGratitudeItem = document.createElement(`li`);
     newGratitudeItem.textContent = input;
     gratitudeList.appendChild(newGratitudeItem);
-    gratitudeInput.value = ` `;
+    gratitudeInput.value = ` `;   
+    }                                  //problem: my list only prevents an empty input at the beginning
 }
 
 function handleMouseOver(e){
     e.target.style.color = "green";
 }
+
+function handleSave(e){
+    let savedItems = document.getElementsByTagName(`ul`);
+    const savedItemsArray = [];
+    for (let i = 0; i < savedItems.length; i++){
+        savedItemsArray.push(savedItems[i].textContent);
+    }
+    localStorage.setItem(`savedListItems`, JSON.stringify(savedItemsArray));
+    const savedData = localStorage.getItem(`savedListItems`)
+    if (savedData){
+        const parseData = JSON.parse(savedData);
+    }
+} 
