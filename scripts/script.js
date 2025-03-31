2464a4b75930e048f1c9348094c50aab9eaab3f6
m-meno <mackenzie.meno@gmail.com>
2025-03-31 13:52:56 -0600

    created basic input, gotElementById, querySelector, and added eventListener

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,41 @@
-//document.selectElementById();
 
-//document.querySelectorAll();
 
-//createElement();
-
-//appendChild || //prependChild
 
 //Document fragment interface???? (only 2%);
 
-//use innerHTML, textContent or innerText
 
 //Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
 
 //ex: adjust style of a button after it has been clicked
 
-//BOM properties
-// window.alert(message): Displays a message to the user. 
-// window.confirm(message): Asks the user for confirmation (OK/Cancel). 
-//window.prompt
 
 
+window.confirm("Welcome, human! Press 'OK' to start recording your gratitude now.")
+
+let gratitudeInput = document.getElementById("gratitudeInput");
+let gratitudeForm = document.getElementById("gratitudeForm");
+let gratitudeList = document.getElementById("gratitudeList");
+let headers = document.querySelector("h1");
+
+headers.addEventListener(`mouseover`, handleMouseOver);
+
+gratitudeForm.addEventListener(`submit`, handleSubmit);
+
+
+
+function handleSubmit(e){
+    e.preventDefault();
+    let input = gratitudeInput.value;
+    if (input == ``){
+        alert(`No input received`)
+        return;
+    }
+    let newGratitudeItem = document.createElement(`li`);
+    newGratitudeItem.textContent = input;
+    gratitudeList.appendChild(newGratitudeItem);
+    gratitudeInput.value = ` `;
+}
+
+function handleMouseOver(e){
+    e.target.style.color = "green";
+}
