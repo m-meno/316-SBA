810d6b457cdd7b0952b5ecdc1210dc52446cca3e
m-meno <mackenzie.meno@gmail.com>
2025-03-31 11:52:50 -0600

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+//document.selectElementById();
+
+//document.querySelectorAll();
+
+//createElement();
+
+//appendChild || //prependChild
+
+//Document fragment interface???? (only 2%);
+
+//use innerHTML, textContent or innerText
+
+//Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+
+//ex: adjust style of a button after it has been clicked
+
+//BOM properties
+// window.alert(message): Displays a message to the user. 
+// window.confirm(message): Asks the user for confirmation (OK/Cancel). 
+//window.prompt
+
+
